33a8e10fd09f8ae1cad3502ac407569c6ee901b2
DIVYA SIVAKUMAR <112675595+divyyeahhhhh@users.noreply.github.com>
2022-10-07 13:50:22 +0530

    Add files via upload

---
+++ OUTPUTDAY3PROGRAM5.py
@@ -0,0 +1,61 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/DAY3PROGRAM5.py 
+Enter the size of the list :11
+Enter the element :1
+Enter the element :3
+Enter the element :5
+Enter the element :8
+Enter the element :9
+Enter the element :2
+Enter the element :6
+Enter the element :7
+Enter the element :6
+Enter the element :8
+Enter the element :9
+Minimum number of jumps to reach end is 3
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/DAY3PROGRAM5.py 
+Enter the size of the list :11
+Enter the element :1
+Enter the element :1
+Enter the element :1
+Enter the element :1
+Enter the element :1
+Enter the element :1
+Enter the element :1
+Enter the element :1
+Enter the element :1
+Enter the element :1
+Enter the element :1
+Minimum number of jumps to reach end is 10
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/DAY3PROGRAM5.py 
+Enter the size of the list :5
+Enter the element :2
+Enter the element :3
+Enter the element :1
+Enter the element :1
+Enter the element :4
+Minimum number of jumps to reach end is 2
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/DAY3PROGRAM5.py 
+Enter the size of the list :6
+Enter the element :1
+Enter the element :3
+Enter the element :6
+Enter the element :1
+Enter the element :0
+Enter the element :9
+Minimum number of jumps to reach end is 3
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/DAY3PROGRAM5.py 
+Enter the size of the list :5
+Enter the element :2
+Enter the element :3
+Enter the element :0
+Enter the element :1
+Enter the element :4
+Minimum number of jumps to reach end is 2
+>>> 
