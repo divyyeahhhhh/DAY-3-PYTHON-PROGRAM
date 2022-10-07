15e31b846839efeb7b38b0c460bc06cce8ef0b5e
DIVYA SIVAKUMAR <112675595+divyyeahhhhh@users.noreply.github.com>
2022-10-07 13:30:54 +0530

    Add files via upload

---
+++ OUTPUTDAY3PROGRAM3.py
@@ -0,0 +1,50 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/DAY3PROGRAM3.py 
+Enter the no. of elements: 6
+Enter the value: 
+1
+2
+3
+1
+1
+3
+Good pairs:  4
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/DAY3PROGRAM3.py 
+Enter the no. of elements: 4
+Enter the value: 
+1
+1
+1
+1
+Good pairs:  6
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/DAY3PROGRAM3.py 
+Enter the no. of elements: 3
+Enter the value: 
+1
+2
+3
+Good pairs:  0
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/DAY3PROGRAM3.py 
+Enter the no. of elements: 5
+Enter the value: 
+4
+5
+1
+3
+7
+Good pairs:  0
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/DAY3PROGRAM3.py 
+Enter the no. of elements: 4
+Enter the value: 
+1
+2
+2
+3
+Good pairs:  1
+>>> 
